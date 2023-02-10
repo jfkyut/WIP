a343987e72c3737411e94e3ac5cbc64094644250
jfkyut <114372518+jfkyut@users.noreply.github.com>
2023-02-11 00:10:39 +0800

    2/11/23

---
+++ script.js
@@ -0,0 +1,40 @@
+// ---------------------------------about------------------------------
+
+const autobioNav = document.querySelector('#autobiography');
+const hobbiesNav = document.querySelector('#hobbies');
+const skillsNav = document.querySelector('#skills');
+
+const autobiography = document.querySelector('.autobiography');
+const hobbies = document.querySelector('.hobbies');
+const skills = document.querySelector('.skills');
+
+// events
+autobioNav.addEventListener('click', () => {
+  autobioNav.classList = 'about-nav active';
+  hobbiesNav.classList = 'about-nav';
+  skillsNav.classList = 'about-nav'
+
+  autobiography.style.display = 'block';
+  hobbies.style.display = 'none';
+  skills.style.display = 'none';
+});
+
+hobbiesNav.addEventListener('click', ()=> {
+  autobioNav.classList = 'about-nav';
+  hobbiesNav.classList = 'about-nav  active';
+  skillsNav.classList = 'about-nav';
+
+  autobiography.style.display = 'none';
+  hobbies.style.display = 'block';
+  skills.style.display = 'none';
+});
+
+skillsNav.addEventListener('click', ()=> {
+  autobioNav.classList = 'about-nav';
+  hobbiesNav.classList = 'about-nav';
+  skillsNav.classList = 'about-nav active';
+
+  autobiography.style.display = 'none';
+  hobbies.style.display = 'none';
+  skills.style.display = 'block';
+});
